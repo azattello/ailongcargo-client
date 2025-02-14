6c64eeeaa2f6d8f994e565cb9617156975e52864
Abdykali Azat <azat.abdykalli@gmail.com>
2025-02-14 17:42:17 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://ailongcargo.kz:3001',
+   
+    nameCargo: 'AILONG CARGO'
+  };
+  
+  
+export default config;
